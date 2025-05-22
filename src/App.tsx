54f9e8044245f69490b11dceefa0e4b5be68d583
Tatchagon Koonkoei <o_k_t@hotmail.com>
2025-05-22 17:44:23 +0700

    create restaurants state from recoil and use in restaurants index

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { RecoilRoot } from "recoil";
 import './App.css'
 
 import RestaurantsIndex from "./components/restaurants/index"
 
 function App() {
   return (
-    <>
+    <RecoilRoot>
       <h1 className="text-4xl font-bold underline mb-4">
         Restaurants
       </h1>
@@ -14,7 +15,7 @@ function App() {
       <div className="divider mt-4 mb-4">
         ==========================
       </div>
-    </>
+    </RecoilRoot>
   )
 }
 
